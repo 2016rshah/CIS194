d945b8ec6718a392ffd7d6793cd80e2d338085db
Rushi Shah <2016rshah@gmail.com>
2015-06-21 16:25:20 -0400

    Finished lesson

--- 04/lecture/lecture-notes.hs
+++ 04/lecture/lecture-notes.hs
@@ -14,4 +14,16 @@ greaterThan100' = filter (>100)
 --Thus f(g(x)) is written as 
 --(f . g) x
 floorSum :: (RealFrac a) => [a] -> Integer
-floorSum = (floor . sum) 
+floorSum = (floor . sum) --this is currying for an array
+
+--curry applies a function to a tuple formed by the second and third argument
+--curry f x y = f (x, y)
+--uncurry applies a function with two arguments from a tuple
+--uncurry f (x, y) = f x y
+
+--Order function arguments from least to most variation between function calls
+--That means you can curry with the last argument and not even pass it in the function definition
+--Point free tries to get rid of the pointers in function calls
+
+--Folds accumulate a list into a value
+--Check out http://learnyouahaskell.com/higher-order-functions and ctrl-f for folds
