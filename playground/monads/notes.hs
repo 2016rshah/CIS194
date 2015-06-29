14a3b28ee5b3116f838e29edd24b8e13c6657e97
Rushi Shah <2016rshah@gmail.com>
2015-06-28 22:39:55 -0400

    Redid file structure and read up on the basics of monads

---
+++ playground/monads/notes.hs
@@ -0,0 +1,5 @@
+--https://blog.jcoglan.com/2011/03/05/translation-from-haskell-to-javascript-of-selected-portions-of-the-best-introduction-to-monads-ive-ever-read/
+-- A clear explanation in terms of JS (because that is much simpler for me to wrap my head around
+
+--A more generic explanation in terms of haskell:
+--http://stackoverflow.com/a/10245311/3861396
