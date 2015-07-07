7a0d921fd2bf3d9fce1c6f556daf96f247052805
Rushi Shah <2016rshah@gmail.com>
2015-07-06 21:51:32 -0400

    Finished exercise 3 in emacs

--- 05/assignment/Calc.hs
+++ 05/assignment/Calc.hs
@@ -13,3 +13,17 @@ eval (Add x y) = eval x + eval y
 --Exercise 2
 evalStr :: String -> Maybe Integer
 evalStr = maybe Nothing (Just . eval) . parseExp Lit Add Mul
+
+--Exercise 3
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
