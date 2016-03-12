11c0a767f4650038bac8622760bc239b31adad4d
Rushi Shah <2016rshah@gmail.com>
2016-03-11 23:40:37 -0500

    I think I just finished it??

--- 12/assignment/Risk.hs
+++ 12/assignment/Risk.hs
@@ -3,6 +3,9 @@
 module Risk where
 
 import Control.Monad.Random
+import Control.Monad
+import Data.List (sort)
+import Control.Applicative
 
 ------------------------------------------------------------
 -- Die values
@@ -20,6 +23,10 @@ instance Random DieValue where
 die :: Rand StdGen DieValue
 die = getRandom
 
+-- | rolls specified number of dice
+dice :: Int -> Rand StdGen [DieValue]
+dice n = replicateM n die
+
 ------------------------------------------------------------
 -- Risk
 
@@ -33,5 +40,42 @@ getAvailibleTroops (Battlefield attackers defenders) = (as, ds)
   where as = if attackers > 3 then 3 else attackers - 1
         ds = if defenders > 1 then 2 else 1
 
--- battle :: Battlefield -> Rand StdGen Battlefield                                
--- battle (Battlefield attackers defenders) = 
+getRolls :: Army -> Rand StdGen [DieValue]
+getRolls n = fmap (reverse . sort) (dice n)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf@(Battlefield allAs allDs) = afterKilling >>= \ (remAs, remDs) -> return (Battlefield (remAs) (remDs))
+  where
+    (avAs, avDs) = getAvailibleTroops bf
+    aRolls = getRolls avAs
+    dRolls = getRolls avDs
+    matchups = zip <$> aRolls <*> dRolls -- Rand StdGen [(DieValue, DieValue)]
+    afterKilling = fmap (foldr killSoldier (allAs, allDs)) matchups -- Rand StdGen (Army, Army)
+
+killSoldier :: (DieValue, DieValue) -> (Army, Army) -> (Army, Army)
+killSoldier (x, y) (a, b) = if x > y then (a, b-1) else (a-1, b)
+
+-- threeInts :: Rand StdGen (Int, Int, Int)
+-- threeInts =
+--   getRandom >>= \i1 ->
+--   getRandom >>= \i2 ->
+--   getRandom >>= \i3 ->
+--   return (i1,i2,i3)
+--evalRandIO is a way to print Rand StdGen stuff
+
+
+--s <- newStdGen
+--evalRand (battle (Battlefield 2 4)) s
+
+--Exercise 2
+invade :: Battlefield -> Rand StdGen Battlefield
+invade bf = battle bf >>= \ rbf@(Battlefield remAs remDs) -> if remDs > 0 && remAs > 1 then invade rbf else return rbf
+
+success :: [Battlefield] -> Rand StdGen Double
+success bfs = return ((fromIntegral successfulBattles) / (fromIntegral allBattles))
+  where
+    allBattles = (length bfs)
+    successfulBattles = length (filter ((==) 0 . defenders) bfs)
+    
+successProb :: Battlefield -> Rand StdGen Double
+successProb bf = replicateM 1000 (invade bf) >>= success
